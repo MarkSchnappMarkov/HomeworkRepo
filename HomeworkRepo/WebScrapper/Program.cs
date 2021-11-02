5ad977b01759af4a1e64e4403b77d8fbea2af560
markmarkov <mark.markov.bg@gmail.com>
2021-11-02 17:06:49 +0200

    Price Scrapper DONE !

--- HomeworkRepo/WebScrapper/Program.cs
+++ HomeworkRepo/WebScrapper/Program.cs
@@ -5,83 +5,79 @@ using System.IO;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace WebScrapper
 {
     class Program
     {
-        public class Row
-        {
-            public string Title { get; set; }
-        }
-
+        public static HtmlNodeCollection itemsName { get; set; }
+        public static HtmlNodeCollection itemsPrices { get; set; }
         public static HtmlNodeCollection GetProductNames(HtmlDocument doc)
         {
             return doc.DocumentNode.SelectNodes("//a[@class='card-v2-title semibold mrg-btm-xxs js-product-url']");
         }
-
         public static HtmlNodeCollection GetProductPrices(HtmlDocument doc)
         {
             return doc.DocumentNode.SelectNodes("//p[@class='product-new-price']");
         }
 
-        public static List<string> ScrapeProducts(HtmlNodeCollection itemsName, HtmlNodeCollection itemsPrices)
+        static void Main(string[] args)
         {
-            string[] items = System.IO.File.ReadAllLines("ItemsToScrape.csv");
+            #region LoadingSite
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://www.emag.bg/mishki/stock/filter/tip-f6329,gejming-v23008/sort-pricedesc/c");
+
+            #endregion
+
+            #region Start
+
+            Task first = new Task(
+                () =>
+                {
+                    itemsName = GetProductNames(doc);
+                    itemsPrices = GetProductPrices(doc);
+                }
+                );
+            first.Start();
+
+            #endregion
+
+            #region FanOut
+
+            first.Wait();
+            string[] wantedItems = System.IO.File.ReadAllLines("ItemsToScrape.csv");
+            List<Task<string>> taskList = new List<Task<string>>();
             List<string> products = new List<string>();
 
             for (int i = 0; i < 5; i++)
             {
-                foreach (var item in itemsName)
+                int index = i;
+                taskList.Add(Task.Run(() =>
                 {
-                    if (item.InnerText == items[i])
-                    {
-                        string title = item.InnerText.Trim();
-                        string rawPrice = itemsPrices[i].InnerText;
-                        string formatedPrice = rawPrice.Remove(rawPrice.Length - 4, 4).ToString();
-                        double finalPrice = Convert.ToDouble(formatedPrice) / 100;
+                    HtmlNode node = itemsName.Where(x => x.InnerText == wantedItems[index]).First();
+                    string title = node.InnerText.Trim();
+                    string rawPrice = itemsPrices[index].InnerText;
+                    string formatedPrice = rawPrice.Remove(rawPrice.Length - 4, 4).ToString();
+                    double finalPrice = Convert.ToDouble(formatedPrice) / 100;
+                    string itemInfo = $"Product: {title} , price: {finalPrice}лв.";
 
-                        products.Add($"Product: {title} , price: {finalPrice}лв.");
-                    }
-                }
+                    products.Add(itemInfo);
+                    return itemInfo;
+                }));
             }
 
-            return products;
-        }
+            #endregion
 
-        public static void CreateReadPrintCSV(List<string> products)
-        {
-            var rows = new List<Row>();
-            foreach (var item in products)
-            {
-                rows.Add(new Row { Title = item });
-            }
-            using (var writer = new StreamWriter("result.csv"))
-            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-            {
-                csv.WriteRecords(rows);
-            }
+            #region FanIn
 
-            string[] print = System.IO.File.ReadAllLines("result.csv");
-            foreach (string line in print)
-            {
-                string[] columns = line.Split(',');
-                foreach (string column in columns)
-                {
-                    Console.WriteLine(column);
-                }
-            }
-        }
+            var fanInTask = Task.WhenAll(taskList);
+            var final = fanInTask.ContinueWith(prev => Console.WriteLine(string.Join("\n", prev.Result)));
+            final.Wait();
+            Console.ReadLine();
 
-        static void Main(string[] args)
-        {
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument doc = web.Load("https://www.emag.bg/mishki/stock/filter/tip-f6329,gejming-v23008/c?ref=lst_leftbar_6408_stock");
-            HtmlNodeCollection itemsName = GetProductNames(doc);
-            HtmlNodeCollection itemsPrices = GetProductPrices(doc);
-
-            CreateReadPrintCSV(ScrapeProducts(itemsName, itemsPrices));
-
+            #endregion
         }
     }
 }
